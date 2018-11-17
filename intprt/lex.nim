fa130aeb410b7f49883a57e287bee107c03d41e6
CreativeGP <malicktafaminecraft@gmail.com>
2018-11-17 20:58:13 +0900

    Defined Token and fix tokenizer to use it

--- intprt/lex.nim
+++ intprt/lex.nim
@@ -3,12 +3,25 @@ import sets
 
 
 # Type sould be a set of entities which it abstructs.
-# However we use strings denoting sets becuase the size of set
+# However I use strings denoting sets becuase the size of set
 # may be infinity (e.g. int)
+# 
+# TODO: The basic set operation function is used to create new type.
+# However, for infinite types, special (internal) implementaion must be needed.
+# I must think about how to create type and deal with infinite types.
 type
   NaturalType = string
   Type = HashSet[NaturalType]
   TypeAnnotation = seq[Type]
+
+# TODO: Token type. Utilize pointers.
+type
+  Token = string
+  Stmt = seq[Token]
+  Fn = ref object
+    ty : TypeAnnotation
+    body : seq[Stmt]
+    args : seq[Fn]
 
 
 const Specials = "!#$%&()-^\\@[;:],./=~|`{+*}<>?"
@@ -22,16 +35,16 @@ proc ctos(ch : char) : string =
   res[0] = ch
   return res
 
-proc addToken(tokens : var seq[string], value : string) =
+proc addToken(tokens : var seq[Token], value : string) =
   if value == "": return
   tokens.add value
 
-proc tokenize*(code : string) : seq[string] =
-  var tokens : seq[string] = @[]
+proc tokenize*(code : string) : seq[Token] =
+  var tokens : seq[Token] = @[]
   var little = ""
   var mode = 0
   
-  for i in 0..<code.len:
+  for i in 0 .. <code.len:
     if mode == 0:
       if Split.contains(code[i]):
         tokens.addToken little
